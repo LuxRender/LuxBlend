2079acd24813bf37d62eb793215176b53cb2b8bc
Tom Bech <tom.bech@gmail.com>
2009-09-29 20:51:10 +0200

    Add tooltip explaining bumpmap scale is in meters (and that consequently 0.01 = 1 cm)

--- LuxBlend_0.1.py
+++ LuxBlend_0.1.py
@@ -5150,7 +5150,7 @@ def luxMaterialBlock(name, luxname, key, mat, gui=None, level=0, str_opt=""):
             usebump = luxProp(mat, keyname+".usebump", "false")
             luxCollapse("usebump", usebump, "Bump Map", "Enable Bump Mapping options", gui, 2.0)
             if usebump.get() == "true":
-                (str,link) = c((str,link), luxFloatTexture("bumpmap", keyname, 0.0, -1.0, 1.0, "bumpmap", "", mat, gui, level+1))
+                (str,link) = c((str,link), luxFloatTexture("bumpmap", keyname, 0.0, -1.0, 1.0, "bumpmap", "bumpmap scale in meters - i.e. 0.01 = 1 cm", mat, gui, level+1))
 
         # emission options (common)
         if (level == 0):
