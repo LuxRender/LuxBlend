241f1df8688357d521c7570be51e28329487d3b9
Jens Verwiebe <devnull@localhost>
2011-05-18 18:34:39 +0200

    Reactivate database DL for myts by removing test URL

--- LuxBlend_0.1.py
+++ LuxBlend_0.1.py
@@ -7899,7 +7899,7 @@ try:
         if id.isalnum():
             DrawProgressBar(0.0,'Getting Material #'+id)
             try:
-                HOST = 'webserver' #'www.luxrender.net'
+                HOST = 'www.luxrender.net'
                 GET = '/lrmdb/en/material/download/'+id+'/'+LBX_VERSION
                 PORT = 80
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8034,7 +8034,7 @@ try:
     # LRMDB Integration
     #===========================================================================
     class lrmdb:
-        host              = 'http://webserver/lrmdb/ixr' #'http://www.luxrender.net/lrmdb/ixr'
+        host              = 'http://www.luxrender.net/lrmdb/ixr'
         
         username          = ""
         password          = ""
